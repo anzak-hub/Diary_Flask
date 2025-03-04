26c788f13155de39d00b593e56c73038ca8269f8
Sebastiano di Gauso <sebastian.gaus@enders-training.de>
2025-03-04 10:55:37 +0100

    First successful app run

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
+# Definere den Pfad für die Home Page
+@app.route('/', methods=["GET", "POST"])
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True, use_reloader=False)
