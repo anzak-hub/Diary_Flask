65b11c918826b5a1f24a7864252efb233f7d1355
Sebastiano di Gauso <sebastian.gaus@enders-training.de>
2025-03-04 11:30:56 +0100

    added styles so it looks nicer - at least for me

--- app.py
+++ app.py
@@ -4,24 +4,25 @@ app = Flask(__name__)
 
 def categorize_bmi(bmi):
     if bmi < 18.5:
-        return "you are underweight - eat more!"
+        return "you are underweight - eat more!", "too-thin"
     elif bmi < 25:
-        return "you have normal weight - keep on going!"
+        return "you have normal weight - keep on going!", "normal"
     else:
-        return "you are overweight - eat less and move more!"
+        return "you are overweight - eat less and move more!", "too-fat"
 
 # Definere den Pfad für die Home Page
 @app.route('/', methods=["GET", "POST"])
 def index():
     bmi = None
     cat = None
+    classy = None
     if request.method == 'POST':
         height = float(request.form["height"])
         weight = float(request.form["weight"])
         bmi = round(weight / (height/100)**2, 2)
-        cat = categorize_bmi(bmi)
+        cat, classy = categorize_bmi(bmi)
     
-    return render_template('index.html', bmi=bmi, category=cat)
+    return render_template('index.html', bmi=bmi, category=cat, classy=classy)
 
 if __name__ == "__main__":
     app.run(debug=True, use_reloader=False)
