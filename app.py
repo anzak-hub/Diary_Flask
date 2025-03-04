fd8814319d6c6d476218cdf27b5e8e6e77949b10
Sebastiano di Gauso <sebastian.gaus@enders-training.de>
2025-03-04 11:19:48 +0100

    main app functionality implemented

--- app.py
+++ app.py
@@ -2,10 +2,26 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+def categorize_bmi(bmi):
+    if bmi < 18.5:
+        return "you are underweight - eat more!"
+    elif bmi < 25:
+        return "you have normal weight - keep on going!"
+    else:
+        return "you are overweight - eat less and move more!"
+
 # Definere den Pfad für die Home Page
 @app.route('/', methods=["GET", "POST"])
 def index():
-    return render_template('index.html')
+    bmi = None
+    cat = None
+    if request.method == 'POST':
+        height = float(request.form["height"])
+        weight = float(request.form["weight"])
+        bmi = round(weight / (height/100)**2, 2)
+        cat = categorize_bmi(bmi)
+    
+    return render_template('index.html', bmi=bmi, category=cat)
 
 if __name__ == "__main__":
     app.run(debug=True, use_reloader=False)
